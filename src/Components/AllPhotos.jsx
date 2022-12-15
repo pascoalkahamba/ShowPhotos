2bd8adaa8929bfdb46d2e5641c97f6d2f280e089
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-15 15:12:24 +0100

    create the states photos and loadiang for make the fetch the pictures

--- src/Components/AllPhotos.jsx
+++ src/Components/AllPhotos.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./AllPhotos.module.css";
 
 const AllPhotos = () => {
+  const [photos, setPhotos] = useState(null);
+  const [loading, setLoading] = useState(null);
+  useEffect(() => {
+    async function getPhotos() {
+      const response = await fetch(
+        "https://ranekapi.origamid.dev/json/api/produto"
+      );
+      setLoading(true);
+      const json = await response.json();
+      console.log(json);
+      setPhotos(json);
+    }
+    getPhotos();
+  }, []);
   return <section className={styles.photos}></section>;
 };
 
