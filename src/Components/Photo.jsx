4418fafbbfb921f3d78e7ac7ba19ddf91e1fe775
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-19 16:02:38 +0100

    create the state picture for make fetch on the component Photo

--- src/Components/Photo.jsx
+++ src/Components/Photo.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
-const Photo = () => {
+const Photo = ({ src, title, id }) => {
+  const [picture, setPicture] = useState(null);
   return (
     <div>
-      <Link to="/"></Link>
+      <Link to={`/produto/${id}`}>
+        <img src={src} title={title} />
+      </Link>
     </div>
   );
 };
