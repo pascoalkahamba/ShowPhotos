bd5e61c1c8c5c574d3cca5c43f3064006794b3b9
pascoalkahamba <pascoalkahamba25@gmail.com>
2022-12-21 13:00:23 +0100

    I created the style component for Contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={styles.contact}>
+      <div className={styles.picture}>
+        <img src="" alt="photo of the contact" />
+      </div>
+      <div className={styles.data}></div>
+    </div>
+  );
 };
 
 export default Contact;
